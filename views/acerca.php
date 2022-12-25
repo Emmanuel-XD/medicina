5fb6fa6075ef375c8cb7a0ce92f6e0a172618bc3
Emmanuel <55505148+Emmanuel-XD@users.noreply.github.com>
2022-12-25 04:08:06 -0500

    Update

--- views/acerca.php
+++ views/acerca.php
@@ -40,33 +40,15 @@ $varsesion = $_SESSION['nombre'];
                             <div class="table-responsive">
 
 
-        <h1>Sistema Administrativo de Citas Medicas "Medical PC" Version 1.0</h1>
+        <h1>Sistema Administrativo de Citas Medicas </h1>
 		<br>
 
-        <p> Medical PC es un Sistema Administrativo de Citas Medicas Web la su funcion principal
+        <p> El Sistema Administrativo de Citas Medicas Web su funcion principal
 			es llevar una mejor gestion sobre las operaciones administrativas que se tiene en un consultorio
 			medico, de esta manera se lleva un mejor control de las citas medicas atravez de este sistema .
 		</p>
 			<br>
 
-        <h2>Sobre el Desarrollador</h2>
-		<br>
-        <p>Desarrollado y Mantenido por <a  href="https://softcodeweb.mysistemventa.com/index.php">SoftCodEPM	</a>.
-            Este sistema es exclusivo para la venta e implementacion del publico en general.
-        
-            También te invitamos a suscribirte a nuestro canal de  <a href="https://www.youtube.com/channel/UCVmGqUX5HUzI3HS3LYwZqHA/videos">Youtube</a>.
-        </p>
-
-		<br>
-
-		<h2>Contacto</h2>
-
-		<br>
-        <p>
-            ¿Quieres contactarnos para alguna duda, queja o comentario?
-			</p>
-
-	      <a href="https://softcodeweb.mysistemventa.com/views/index.php#contact-us">Presiona Aqui </a> 
          
 			
 				<?php include "../includes/footer.php";?>
