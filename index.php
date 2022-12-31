0b6efec0d7540fd3b72be60eb863dcd1384a989b
Emmanuel <55505148+Emmanuel-XD@users.noreply.github.com>
2022-12-31 15:10:36 -0600

    UpdateIndex

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
-
 <?php
 session_start();
 error_reporting(0);
-	$varsesion = $_SESSION['nombre'];
-    $typeUser = $_SESSION['nombre'];
-	if($varsesion== null || $varsesion= '' && $typeUser == null || $typeUser == ''){
+$varsesion = $_SESSION['nombre'];
+$typeUser = $_SESSION['nombre'];
+if ($varsesion == null || $varsesion = '' && $typeUser == null || $typeUser == '') {
 
-   
+
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
+
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
@@ -20,42 +20,59 @@ error_reporting(0);
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="./css/estilo.css" rel="stylesheet" />
+        <link href="./vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 
-        
-  <script src="./js/cont/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.min.js"></script>
-  <script src="./js/cont/bootstrap.min.js"></script>
-  <style>
-    a{
-        text-decoration: none;
-    }
-  </style>
+        <script src="./js/cont/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.min.js"></script>
+        <script src="./js/cont/bootstrap.min.js"></script>
+        <style>
+            a {
+                text-decoration: none;
+            }
+
+            .log {
+                padding-right: 20px;
+                height: 50px;
+                width: 70px;
+            }
+
+            .bar {
+                border: 0;
+                vertical-align: middle;
+                height: auto;
+                max-width: 100%;
+                outline: 0 none;
+
+            }
+        </style>
 
     </head>
+
     <body id="page-top">
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-success fixed-top" id="mainNav">
             <div class="container px-4">
-                <a class="navbar-brand" href="#page-top">IMSS-BIENESTAR</a>
+                <img src="./img/imss.png" class="log" width="50px" height="50px" alt="">
+                <a class="navbar-brand" href="#page-top"> IMSS-BIENESTAR</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
                         <li class="nav-item"><a class="nav-link" href="#about">Home</a></li>
-        
-     
-                        
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#acceso" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Acceso
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item"  data-toggle="modal" data-target="#user">Administracion</a></li>
-            <li><a class="dropdown-item"  data-toggle="modal" data-target="#login">Usuario</a></li>
 
-          </ul>
-        </li>
 
-    
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#acceso" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Acceso
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" data-toggle="modal" data-target="#user">Administracion</a></li>
+                                <li><a class="dropdown-item " data-toggle="modal" data-target="#login">Usuario</a></li>
+                                <li><a class="dropdown-item" href="./views/index.php"></a></li>
+                            </ul>
+                        </li>
+
+
                     </ul>
                 </div>
             </div>
@@ -64,46 +81,46 @@ error_reporting(0);
         <header class="bg-dark bg-gradient text-white">
             <div class="container px-4 text-center">
                 <h1 class="fw-bolder"> INSTITUTO MEXICANO DEL SEGURO SOCIAL</h1>
-               <br>
-                <a class="btn btn-lg btn-light" href="#about">Start scrolling!</a>
+
             </div>
         </header>
+
         <!-- About section-->
         <section id="about">
             <div class="container px-4">
                 <div class="row gx-4 justify-content-center">
                     <div class="col-lg-8">
-                        <h2>About this page</h2>
-                        <p class="lead">This is a great place to talk about your webpage. This template is purposefully unstyled so you can use it as a boilerplate or starting point for you own landing page designs! This template features:</p>
-                        <ul>
-                            <li>Clickable nav links that smooth scroll to page sections</li>
-                            <li>Responsive behavior when clicking nav links perfect for a one page website</li>
-                            <li>Bootstrap's scrollspy feature which highlights which section of the page you're on in the navbar</li>
-                            <li>Minimal custom CSS so you are free to explore your own unique design options</li>
-                        </ul>
+                        <h2>IMSS-BIENESTAR</h2>
+                        <br>
+                        <img src="./img/barra.jpg" class="bar" alt="">
+                        <br>
+                        <br>
+                        <h3>Introduccion</h3>
+
+                        <p>Con 43 años de experiencia, actualmente IMSS-BIENESTAR cuenta con una amplia red de servicios donde se conjuga la atención médica con las acciones de promoción a la salud en la propia comunidad.</p>
                     </div>
                 </div>
             </div>
         </section>
         <!-- Footer-->
-        <footer class="py-5 bg-dark">
-            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2022</p></div>
+        <footer class="py-5 bg-dark bg-gradient text-white">
+            <div class="container px-4">
+                <p class="m-0 text-center text-white">Copyright &copy; Sistema De Citas Medicas 2022</p>
+            </div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
         <script src="./js/scripts.js"></script>
 
-        
+
     </body>
-    <?php include "./home/account.php";?>
-    <?php include "./includes/_sesion/login.php";?>
+    <?php include "./home/account.php"; ?>
+    <?php include "./includes/_sesion/login.php"; ?>
     <script src="./js/logins.js"></script>
-</html>
-<?php  }
 
-
-else{
+    </html>
+<?php  } else {
     header("Location: views/index.php");
 }
 ?>
