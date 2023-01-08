282c3704d79591d520426826d541b309964f97ef
Emmanuel <55505148+Emmanuel-XD@users.noreply.github.com>
2023-01-08 10:38:39 -0600

    Update

--- includes/reset-password.php
+++ includes/reset-password.php
@@ -15,34 +15,33 @@
     <title>Recuperar contraseña</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-success fixed-top" id="mainNav">
-            <div class="container px-4">
-                <img src="../img/imss.png" class="log" width="50px" height="50px" alt="">
-                <a class="navbar-brand" href="#page-top"> IMSS-BIENESTAR</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link" href="#about">Home</a></li>
 
-
-
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#acceso" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Acceso
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" data-toggle="modal" data-target="#user">Administración</a></li>
-                                <li><a class="dropdown-item " data-toggle="modal" data-target="#login">Usuario</a></li>
-                            </ul>
-                        </li>
-
-
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-
+        <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+include "db.php";
+$error = '';
+if (isset($_GET["key"]) && isset($_GET["email"]) && isset($_GET["action"]) && ($_GET["action"]=="reset") && !isset($_POST["action"])){
+  $key = $_GET["key"];
+  $email = $_GET["email"];
+  $curDate = date("Y-m-d H:i:s");
+  $sql = "SELECT * FROM `password_reset_temp` WHERE `key`='".$key."' and `email`='".$email."';";
+  $query = mysqli_query($conexion,$sql);
+  $row = mysqli_num_rows($query);
+  if ($row==""){
+  $error .= '<h2>Invalid Link</h2>
+<p>The link is invalid/expired. Either you did not copy the correct link
+from the email, or you have already used the key in which case it is 
+deactivated.</p>
+<p><a href="https://www.allphptricks.com/forgot-password/index.php">
+Click here</a> to reset password.</p>';
+	}else{
+  $row = mysqli_fetch_assoc($query);
+  $expDate = $row['expDate'];
+  if ($expDate >= $curDate){
+  ?>
+  
 <div class="formreset container h-100">
   <div class="row h-100 justify-content-center align-items-center">
     <div class="col-10 col-md-8 col-lg-6">
@@ -70,32 +69,7 @@
     </div>
   </div>
 </div>
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-include "db.php";
-$error = '';
-if (isset($_GET["key"]) && isset($_GET["email"]) && isset($_GET["action"]) && ($_GET["action"]=="reset") && !isset($_POST["action"])){
-  $key = $_GET["key"];
-  $email = $_GET["email"];
-  $curDate = date("Y-m-d H:i:s");
-  $sql = "SELECT * FROM `password_reset_temp` WHERE `key`='".$key."' and `email`='".$email."';";
-  $query = mysqli_query($conexion,$sql);
-  $row = mysqli_num_rows($query);
-  if ($row==""){
-  $error .= '<h2>Invalid Link</h2>
-<p>The link is invalid/expired. Either you did not copy the correct link
-from the email, or you have already used the key in which case it is 
-deactivated.</p>
-<p><a href="https://www.allphptricks.com/forgot-password/index.php">
-Click here</a> to reset password.</p>';
-	}else{
-  $row = mysqli_fetch_assoc($query);
-  $expDate = $row['expDate'];
-  if ($expDate >= $curDate){
-  ?>
-  
+
 <?php
 }else{
 $error .= "<h2>Link Expired</h2>
