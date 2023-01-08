50ab774891abc74c6aaa3af21dc344d623036a35
Emmanuel <55505148+Emmanuel-XD@users.noreply.github.com>
2023-01-08 10:22:22 -0600

    UpdateReset-Password

--- includes/reset-password.php
+++ includes/reset-password.php
@@ -1,3 +1,75 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="../css/estilo.css" rel="stylesheet" />
+        <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="../css/reset.css">
+        <script src="../js/cont/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.min.js"></script>
+        <script src="../js/cont/bootstrap.min.js"></script>
+    <title>Recuperar contraseña</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-success fixed-top" id="mainNav">
+            <div class="container px-4">
+                <img src="../img/imss.png" class="log" width="50px" height="50px" alt="">
+                <a class="navbar-brand" href="#page-top"> IMSS-BIENESTAR</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link" href="#about">Home</a></li>
+
+
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#acceso" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Acceso
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" data-toggle="modal" data-target="#user">Administración</a></li>
+                                <li><a class="dropdown-item " data-toggle="modal" data-target="#login">Usuario</a></li>
+                            </ul>
+                        </li>
+
+
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+
+<div class="formreset container h-100">
+  <div class="row h-100 justify-content-center align-items-center">
+    <div class="col-10 col-md-8 col-lg-6">
+
+      <form class="form-ex" action="" method="post">
+        <h1>RESTAURAR CONTRASEÑA</h1>
+        <div class="alerts">
+
+        </div>
+        <p class="description">Introduce tu nueva contraseña.</p>
+        
+
+        <form method="post" action="" name="update">
+          <input type="hidden" name="action" value="update" />
+
+          <label><strong>Nueva Contraseña:</strong></label>
+          <input type="password" name="pass1" class="form-control username" maxlength="15" required />
+
+          <label><strong>Confirmar Contraseña:</strong></label><br />
+          <input type="password" class="form-control username" name="pass2" maxlength="15" required />
+
+          <input type="hidden" name="email" value="<?php echo $email; ?>" />
+          <input type="submit" value="Reset Password" class="btn btn-primary btn-customized" />
+        </form>
+    </div>
+  </div>
+</div>
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -23,19 +95,7 @@ Click here</a> to reset password.</p>';
   $expDate = $row['expDate'];
   if ($expDate >= $curDate){
   ?>
-  <br />
-  <form method="post" action="" name="update">
-  <input type="hidden" name="action" value="update" />
-  <br /><br />
-  <label><strong>Enter New Password:</strong></label><br />
-  <input type="password" name="pass1" maxlength="15" required />
-  <br /><br />
-  <label><strong>Re-Enter New Password:</strong></label><br />
-  <input type="password" name="pass2" maxlength="15" required/>
-  <br /><br />
-  <input type="hidden" name="email" value="<?php echo $email;?>"/>
-  <input type="submit" value="Reset Password" />
-  </form>
+  
 <?php
 }else{
 $error .= "<h2>Link Expired</h2>
@@ -73,4 +133,3 @@ echo '<div class="error"><p>Congratulations! Your password has been updated succ
 Click here</a> to Login.</p></div><br />';
 	  }		
 }
-?>
